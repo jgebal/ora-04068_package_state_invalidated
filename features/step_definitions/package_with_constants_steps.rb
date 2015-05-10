6cacd6546d9b13cb936c4d7ac53f90c03bd9fe68
Jacek Gębal <jgebal@gmail.com>
2015-05-10 22:51:29 +0100

    Added pending implementation for feature

---
+++ features/step_definitions/package_with_constants_steps.rb
@@ -0,0 +1,15 @@
+require_relative '../../config/common_helper'
+
+Given(/^I have accessed a package containing constants$/) do
+  pending
+end
+
+
+When(/^The package is recompiled in "([^"]*)" session$/) do |session|
+  pending
+end
+
+Then(/^Accessing the package will raise ORA-04068 exception$/) do
+  pending
+end
+
